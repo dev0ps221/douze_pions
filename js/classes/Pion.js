941446ee9886ce2ce626a8e8c03dbd05ff04267a
dev0ps221 <seybalofficiel@gmail.com>
2023-01-15 17:36:56 +0000

    douze:implementing possiblemoves

--- js/classes/Pion.js
+++ js/classes/Pion.js
@@ -2,15 +2,39 @@ class Pion{
     joueur  = null;
     couleur = null;
     colonne = null;
+    playerdirection = null;
 
     getPosition(){
         return {x:this.colonne.index,y:this.colonne.ligne.index}
     }
 
     movable(){
-        alert(this.joueur)
+        const rightMoves = this.checkRightMoves()
+        const letfMoves = this.checkLetfMoves()
+        const upmoves = this.checkUpMoves()
+        const downmoves = this.checkDownMoves()
+        alert(this.playerdirection)
     }
+    checkDownMoves(){
+        const moves = []
 
+        return moves
+    }
+    checkUpMoves(){
+        const moves = []
+
+        return moves
+    }
+    checkRightMoves(){
+        const moves = []
+
+        return moves
+    }
+    checkUpMoves(){
+        const moves = []
+        
+        return moves
+    }
     changeColonne(colonne){
         this.colonne = this.colonne
     }
@@ -35,6 +59,7 @@ class Pion{
         this.elem.classList.add('pion')
         this.elem.classList.add(this.couleur)
         this.joueur = joueur;
+        this.playerdirection = joueur == 1 ? 'down' : 'up'
         this.colonne = colonne;
         this.board = this.colonne.board
     }
